36d135f55c54f3eeb7b013673a7f5a50d27f282f
eunjung kim <ujandgd@naver.com>
2020-12-08 16:18:50 +0900

    class, payment DTO수정

--- energym/src/main/java/com/escape/energym/common/dto/Payment.java
+++ energym/src/main/java/com/escape/energym/common/dto/Payment.java
@@ -1,5 +1,14 @@
 package com.escape.energym.common.dto;
 
-public class Payment {
+import java.sql.Date;
 
+import lombok.Data;
+
+@Data
+public class Payment {
+	private int paymentNo;
+	private String memberId;
+	private int ticketNo;
+	private int usableCount;
+	private Date paymentDate;
 }
